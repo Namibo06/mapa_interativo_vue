7f7ecafd43f8753c3d8d5963b5d9a934e39b95df
Namibo06 <namibo654@gmail.com>
2024-12-29 00:09:43 -0300

    feat: created service with mocked data

---
+++ src/services/order.js
@@ -0,0 +1,10 @@
+export const orders = [
+    { id: 1, restaurantLocation: { lat: -12.267, lng: -38.966 }, customerLocation: { lat: -12.256, lng: -38.975 }, deliveryId: 1, customerName: 'João',companyName: 'Restaurante Delícia', completed: false },
+    { id: 2, restaurantLocation: { lat: -12.276, lng: -38.960 }, customerLocation: { lat: -12.270, lng: -38.962 }, deliveryId: 2, customerName: 'Ana', companyName: 'Comida Boa', completed: false },
+    { id: 3, restaurantLocation: { lat: -12.290, lng: -38.950 }, customerLocation: { lat: -12.285, lng: -38.940 }, deliveryId: 1,  customerName: 'Carlos', companyName: 'Restaurante Delícia', completed: false }
+];
+
+export const deliverers = [
+    { id: 1, name: 'Jonathan', currentLocation: { lat: -12.2660, lng: -38.9660 } },
+    { id: 2, name: 'Maria', currentLocation: { lat: -12.2645, lng: -38.9645 } },
+];
