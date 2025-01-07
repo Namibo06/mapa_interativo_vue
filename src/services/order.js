b0e75519e9938368e8c10bec2ffd9e51c2b12079
Namibo06 <namibo654@gmail.com>
2025-01-07 15:40:58 -0300

    feat: added option selected

--- src/services/order.js
+++ src/services/order.js
@@ -5,14 +5,14 @@ export const orders = [
     { id: 4, restaurantLocation: { lat: -12.167, lng: -38.866 }, customerLocation: { lat: -12.156, lng: -38.875 }, deliveryId: 3, customerName: 'Gilmar',companyName: 'Restaurante Delícia', completed: false },
     { id: 5, restaurantLocation: { lat: -12.176, lng: -38.860 }, customerLocation: { lat: -12.070, lng: -38.262 }, deliveryId: 4, customerName: 'Laudicea', companyName: 'Comida Boa', completed: false },
     { id: 6, restaurantLocation: { lat: -12.190, lng: -38.850 }, customerLocation: { lat: -12.125, lng: -38.820 }, deliveryId: null,  customerName: 'Nádia', companyName: 'Restaurante Delícia', completed: false },
-    { id: 7, restaurantLocation: { lat: -12.067, lng: -38.866 }, customerLocation: { lat: -12.056, lng: -38.575 }, deliveryId: 3, customerName: 'Maia',companyName: 'Restaurante Delícia', completed: false },
+    { id: 7, restaurantLocation: { lat: -12.067, lng: -38.366 }, customerLocation: { lat: -12.056, lng: -38.575 }, deliveryId: 3, customerName: 'Maia',companyName: 'Restaurante Delícia', completed: false },
     { id: 8, restaurantLocation: { lat: -12.076, lng: -38.860 }, customerLocation: { lat: -12.270, lng: -38.462 }, deliveryId: 4, customerName: 'Lucas', companyName: 'Comida Boa', completed: false },
     { id: 9, restaurantLocation: { lat: -12.090, lng: -38.850 }, customerLocation: { lat: -12.085, lng: -38.740 }, deliveryId: null,  customerName: 'Nelson', companyName: 'Restaurante Delícia', completed: false }
 ];
 
 export const deliverers = [
-    { id: 1, name: 'Jonathan', currentLocation: { lat: -12.2660, lng: -38.9660 }, color: '#FFFF00' },
-    { id: 2, name: 'Maria', currentLocation: { lat: -12.2645, lng: -38.9645 }, color: '#000000' },
-    { id: 3, name: 'Joca', currentLocation: { lat: -12.2100, lng: -38.9560 }, color: '#0000FF' },
-    { id: 4, name: 'William', currentLocation: { lat: -12.1645, lng: -38.6645 }, color: '#FFA500.' },
+    { id: 1, name: 'Jonathan', currentLocation: { lat: -12.2660, lng: -38.9660 }, color: '#FFFF00', selected: false },
+    { id: 2, name: 'Maria', currentLocation: { lat: -12.2645, lng: -38.9645 }, color: '#000000', selected: false },
+    { id: 3, name: 'Joca', currentLocation: { lat: -12.2100, lng: -38.9560 }, color: '#0000FF', selected: false },
+    { id: 4, name: 'William', currentLocation: { lat: -12.1645, lng: -38.6645 }, color: '#FFA500', selected: false },
 ];
