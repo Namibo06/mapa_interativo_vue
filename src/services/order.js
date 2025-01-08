eb5b9c577604d198859b0e5b7d833b8d2f5b8685
Namibo06 <namibo654@gmail.com>
2025-01-08 11:28:40 -0300

    feat: added more deliverers and changed deliveryId in the orders with id 6 and 9

--- src/services/order.js
+++ src/services/order.js
@@ -4,15 +4,20 @@ export const orders = [
     { id: 3, restaurantLocation: { lat: -12.290, lng: -38.950 }, customerLocation: { lat: -12.185, lng: -38.840 }, deliveryId: 1,  customerName: 'Carlos', companyName: 'Restaurante Delícia', completed: false },
     { id: 4, restaurantLocation: { lat: -12.167, lng: -38.866 }, customerLocation: { lat: -12.156, lng: -38.875 }, deliveryId: 3, customerName: 'Gilmar',companyName: 'Restaurante Delícia', completed: false },
     { id: 5, restaurantLocation: { lat: -12.176, lng: -38.860 }, customerLocation: { lat: -12.070, lng: -38.262 }, deliveryId: 4, customerName: 'Laudicea', companyName: 'Comida Boa', completed: false },
-    { id: 6, restaurantLocation: { lat: -12.190, lng: -38.850 }, customerLocation: { lat: -12.125, lng: -38.820 }, deliveryId: null,  customerName: 'Nádia', companyName: 'Restaurante Delícia', completed: false },
+    { id: 6, restaurantLocation: { lat: -12.190, lng: -38.850 }, customerLocation: { lat: -12.125, lng: -38.820 }, deliveryId: 5,  customerName: 'Nádia', companyName: 'Restaurante Delícia', completed: false },
     { id: 7, restaurantLocation: { lat: -12.067, lng: -38.366 }, customerLocation: { lat: -12.056, lng: -38.575 }, deliveryId: 3, customerName: 'Maia',companyName: 'Restaurante Delícia', completed: false },
     { id: 8, restaurantLocation: { lat: -12.076, lng: -38.860 }, customerLocation: { lat: -12.270, lng: -38.462 }, deliveryId: 4, customerName: 'Lucas', companyName: 'Comida Boa', completed: false },
-    { id: 9, restaurantLocation: { lat: -12.090, lng: -38.850 }, customerLocation: { lat: -12.085, lng: -38.740 }, deliveryId: null,  customerName: 'Nelson', companyName: 'Restaurante Delícia', completed: false }
+    { id: 9, restaurantLocation: { lat: -12.090, lng: -38.850 }, customerLocation: { lat: -12.085, lng: -38.740 }, deliveryId: 5,  customerName: 'Nelson', companyName: 'Restaurante Delícia', completed: false },
+    { id: 10, restaurantLocation: { lat: -12.020, lng: -38.810 }, customerLocation: { lat: -12.020, lng: -38.810 }, deliveryId: 4, customerName: 'Mateus', companyName: 'Comida Boa', completed: false },
+    { id: 11, restaurantLocation: { lat: -12.220, lng: -38.620 }, customerLocation: { lat: -12.220, lng: -38.620 }, deliveryId: 4,  customerName: 'Gustavo', companyName: 'Restaurante Delícia', completed: false },
+    { id: 12, restaurantLocation: { lat: -12.270, lng: -38.462 }, customerLocation: { lat: -12.270, lng: -38.462 }, deliveryId: 5, customerName: 'Fernanda', companyName: 'Comida Boa', completed: false },
+    { id: 13, restaurantLocation: { lat: -12.244, lng: -38.825 }, customerLocation: { lat: -12.244, lng: -38.825 }, deliveryId: 5,  customerName: 'Carol', companyName: 'Restaurante Delícia', completed: false }
 ];
 
 export const deliverers = [
-    { id: 1, name: 'Jonathan', currentLocation: { lat: -12.2660, lng: -38.9660 }, color: '#00FF00', selected: false },
-    { id: 2, name: 'Maria', currentLocation: { lat: -12.2645, lng: -38.9645 }, color: '#000000', selected: false },
-    { id: 3, name: 'Joca', currentLocation: { lat: -12.2100, lng: -38.9560 }, color: '#0000FF', selected: false },
-    { id: 4, name: 'William', currentLocation: { lat: -12.1645, lng: -38.6645 }, color: '#FFA500', selected: false },
+    { id: 1, name: 'Jonathan', currentLocation: { lat: -12.2660, lng: -38.9660 }, color: '#00FF00' },
+    { id: 2, name: 'Maria', currentLocation: { lat: -12.2645, lng: -38.9645 }, color: '#000000' },
+    { id: 3, name: 'Joca', currentLocation: { lat: -12.2100, lng: -38.9560 }, color: '#0000FF' },
+    { id: 4, name: 'William', currentLocation: { lat: -12.1645, lng: -38.6645 }, color: '#FFA500' },
+    { id: 5, name: 'Sem entregador', currentLocation: { lat: -12.0645, lng: -38.9645 }, color: '#808080' },
 ];
