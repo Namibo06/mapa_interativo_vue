6c28518d56f6c2619e6659e0d9cc3e8deda19bb6
Namibo06 <namibo654@gmail.com>
2025-01-10 09:44:21 -0300

    feat: interacting with Test component, changed data in V-For, created fixed colors for couriers, created new methods to update the map and interact with it, and manipulate colors

--- src/App.vue
+++ src/App.vue
@@ -1,71 +1,219 @@
 <template>
-  <main>
-    <div class="couriers_map">
-      <div class="card_couriers">
-          <button type="button" @click="getAllOrdersAndDeliverers">Todos</button>
-          <div class="couriers_options" v-for="courier in deliverers" :key="courier.name">
-            <button 
-              type="button"
-              :id="courier.id"
-              @click="mapDeliverers(courier)"  
-              :class="selectedCouriersState[courier.id] ? 'ball_custom_card_active' : 'ball_custom_card'"
-              :style="
-              'background-color:'+
-                [
-                  selectedCouriersState[courier.id] ?
-                  courier.color :
-                  '#ffffff'
-                ] + ';'+
-                'color:'+ 
-                [
-                  selectedCouriersState[courier.id] ?
-                  '#ffffff' :
-                  '#000000'
-                ]
-              "
-            >
-              <div class="card_custom_option" 
-                v-show="!selectedCouriersState[courier.id]"
-                :style="'background-color:'+ [courier.color] + ';'"></div><!--card_custom_option-->
+  <div>
+    <main>
+      <TesteAtualizacao @atualizar-dados="atualizarDados" />
 
-              <span>{{ courier.name }}</span>
-            </button>
-          </div>
-      </div><!--card_couriers-->
+      <div class="couriers_map">
+        <div class="card_couriers">
+            <button type="button" @click="getAllOrdersAndDeliverers">Todos</button>
+            <div class="couriers_options" v-for="courier in this.arrayPedidosMerge" :key="courier.entregador.entregador_id">
+              <button 
+                type="button"
+                @click="mapDeliverers(courier)"  
+                :class="selectedCouriersState[courier.entregador.ent_nome] ? 'ball_custom_card_active' : 'ball_custom_card'"
+                :style="{
+                  'background-color': selectedCouriersState[courier.entregador?.ent_nome] ? courier.entregador.color : '#ffffff',
+                  'color': selectedCouriersState[courier.entregador.ent_nome] ? '#ffffff' : '#000000'
+                }"
 
-      <Map
-        ref="map"
-        :selectedDeliverer="selectedDeliverer" 
-        :order="selectedOrder" 
-        :selectedMultipleCouriers="selectedMultipleCouriers"
-        
-      />
-    </div><!--couriers_map-->
-  </main>
+              >
+                <div class="card_custom_option" 
+                  @click="changeCardCustomOptionShowHide(courier.entregador.ent_nome)"
+                  v-if="showHideCardCustomOption"
+                  v-show="!selectedCouriersState[courier.entregador.ent_nome]"
+                  :style="'background-color:' +  getCourierColor(courier)"></div>
+                <span>{{courier.entregador.ent_nome}}</span>
+              </button>
+
+            </div><!--couriers_options-->
+        </div><!--card_couriers-->
+
+        <Map
+          ref="map"
+          :selectedDeliverer="selectedDeliverer" 
+          :order="selectedOrder" 
+          :selectedMultipleCouriers="selectedMultipleCouriers"
+          :updatedOrder="updatedOrder"
+        />
+      </div><!--couriers_map-->
+    </main>
+  </div>
 </template>
 
 <script>
 import Map from '@/components/MapComponent.vue';
-import { deliverers } from '@/services/order';
+import { pedidos } from '@/services/order';
+import TesteAtualizacao from './components/TesteAtualizacao.vue';
+
+const fixedColors = ['#FF0000', '#00FF00', '#0000FF', '#FF4500', '#00008B', '#008000', '#FFD700', '#FF1493', '#32CD32']; 
+const courierColorsMap = {}; 
+let colorIndex = 0;
 
 export default {
   components: {
+    TesteAtualizacao,
     Map
   },
   data() {
     return {
-      deliverers,
+      showHideCardCustomOption: true,
+      pedidos,  
       selectedDeliverer: null,
       selectedOrder: null,
       updateMapDeliverers: null,
+      showMap: false,
       selectedCouriers: [],
       selectedMultipleCouriers: [],
       selectedCouriersState: {},
-      clickInAll: false,
-      clickInWithoutCourier: false,
+      arrayPedidosMerge: [],
+      pedidoProducaoArray: [],
+      pedidoSaiuEntregaArray: [],
+      pedidoEntregueArray: [],
+      usedColors: [],
+      arrayPedidosMergeFacility: [],
+      processedEntregador: null,
+      validationEntregador: [],
+      updatedOrder: null
     };
   },
+  mounted(){
+    this.setDeliverersColors();
+  },
+  created(){
+    this.getAllData();
+  },
+  computed: {
+    usedColorsMethod() {
+      return this.$root.usedColors || [];
+    }
+  },
   methods: {
+    atualizarDados(payload) { 
+      const updatePedido = (pedidoArray) => { 
+        const pedidoIndex = pedidoArray.findIndex(pedido => 
+          pedido.pedido_id === payload.idPedido
+        ); 
+        
+        if (pedidoIndex !== -1) { 
+          this.$set(pedidoArray, pedidoIndex, payload); 
+          return true; 
+        } 
+        return false; 
+      };
+
+      const pedidoKeys = ['entregue', 'producao', 'saiuentrega'];
+      let updated = false;
+
+      for (const key of pedidoKeys) {
+        if (updatePedido(this.pedidos[key])) {
+          updated = true;
+          break;
+        }
+      }
+
+      if (!updated) { 
+        this.pedidos.producao.push(payload); 
+      } 
+
+      this.updatedOrder = payload;  
+    },
+
+    changeCardCustomOptionShowHide(courierName) {
+      this.showHideCardCustomOption = !this.showHideCardCustomOption;
+  
+      if (this.showHideCardCustomOption) {
+        this.$refs.map.showPolylines(courierName);  
+      } else {
+        this.$refs.map.hidePolylines(courierName); 
+      }
+    },
+
+    setDeliverersColors() { 
+      this.arrayPedidosMerge.forEach(pedido => { 
+        if (!pedido.entregador.color) { 
+          if (pedido.entregador.ent_nome === "Sem Entregador" ||
+              pedido.entregador.ent_nome === "Sem entregador" || 
+              pedido.entregador.ent_nome === "sem Entregador" || 
+              pedido.entregador.ent_nome === "sem entregador") { 
+            pedido.entregador.color = "#808080"; 
+          } else { 
+            const entregadorId = pedido.entregador.entregador_id; 
+            if (!courierColorsMap[entregadorId]) { 
+              courierColorsMap[entregadorId] = fixedColors[colorIndex]; 
+              colorIndex = (colorIndex + 1) % fixedColors.length; 
+            } 
+            pedido.entregador.color = courierColorsMap[entregadorId]; 
+          } 
+        } 
+      }); 
+    },
+
+    getCourierColor(courier) {
+      if (
+        courier.entregador.ent_nome === "Sem Entregador" ||
+        courier.entregador.ent_nome === "Sem entregador" ||
+        courier.entregador.ent_nome === "sem Entregador" ||
+        courier.entregador.ent_nome === "sem entregador"
+      ) {
+        console.log('aqui tem sem entregador');
+        courier.entregador.color = "#808080";
+      }
+
+      return courier.entregador?.color || '#000000'; 
+    },
+
+    getAllData(){
+        this.pedidoProducaoArray = this.pedidos.producao;
+        this.pedidoSaiuEntregaArray = this.pedidos.saiuentrega;
+        this.pedidoEntregueArray = this.pedidos.entregue;
+
+        this.arrayPedidosMerge = [];
+        /**verificar se pedidoProducaoArray e os demais é undefined */
+
+        this.pedidoProducaoArray.forEach(pedido => {
+          const keyEntregador = `${pedido.entregador.entregador_id}`;
+          this.processedEntregador = new Set();
+
+          if(this.processedEntregador.has(keyEntregador)){
+            return;
+          }
+          this.processedEntregador.add(keyEntregador);
+
+          this.processedEntregador.forEach(entregador => {
+
+            if(parseInt(keyEntregador) !== parseInt(entregador)){
+              console.log('nao é o mesmo');
+              return;
+            }else{
+              this.arrayPedidosMerge.push(pedido);
+            }
+          });
+        });
+
+        this.pedidoSaiuEntregaArray.forEach(pedido => {
+          const keyEntregador = `${pedido.entregador.entregador_id}`;
+
+          if(this.processedEntregador.has(keyEntregador)){
+            return;
+          }
+          this.processedEntregador.add(keyEntregador);
+
+          this.processedEntregador.forEach(entregador => {
+            if(parseInt(keyEntregador) !== parseInt(entregador)){
+              console.log('nao é o mesmo');
+              return;
+            }else{
+              this.arrayPedidosMerge.push(pedido);
+            }
+          });
+        });
+    },
+
+    handleShowRouteOnMap(courier, order) {
+      this.selectedDeliverer = courier;
+      this.selectedOrder = order;
+      this.showMap = true;
+    },
     toggleCourierInSearch(courier) {
       const index = this.selectedCouriers.indexOf(courier);
       if (index === -1) {
@@ -80,85 +228,91 @@ export default {
       this.$refs.map.clearMap();
     },
 
-    mapDeliverers(deliverer) {
-      this.selectedCouriersState[deliverer.id] = !this.selectedCouriersState[deliverer.id];
-
-      if (this.selectedCouriersState[deliverer.id]) {
-        const courierKey = `${deliverer.name}.${deliverer.id}.${deliverer.currentLocation.lat}.${deliverer.currentLocation.lng}.${deliverer.color}`;
-        this.selectedMultipleCouriers.push(courierKey);
+    mapDeliverers(courier) {
+      if (
+        courier.entregador.ent_nome === "Sem Entregador" ||
+        courier.entregador.ent_nome === "Sem entregador" ||
+        courier.entregador.ent_nome === "sem Entregador" ||
+        courier.entregador.ent_nome === "sem entregador"
+      ) {
+        courier.entregador.color = "#808080"; 
       } else {
-        const courierKey = `${deliverer.name}.${deliverer.id}.${deliverer.currentLocation.lat}.${deliverer.currentLocation.lng}.${deliverer.color}`;
-        this.selectedMultipleCouriers = this.selectedMultipleCouriers.filter(item => item !== courierKey);
+        const entregadorId = courier.entregador.entregador_id;
+        
+        if (!courierColorsMap[entregadorId]) {
+          courierColorsMap[entregadorId] = fixedColors[colorIndex];
+          colorIndex = (colorIndex + 1) % fixedColors.length;
+        }
+        
+        courier.entregador.color = courierColorsMap[entregadorId];
       }
 
-      this.$refs.map.addAllOrdersForDeliverers(this.selectedMultipleCouriers);
+      this.selectedCouriersState[courier.entregador.ent_nome] = !this.selectedCouriersState[courier.entregador.ent_nome];
+      this.selectedCouriersState[courier.entregador.entregador_id] = !this.selectedCouriersState[courier.entregador.entregador_id];
+      this.updateMapDeliverers = courier;
+
+      this.selectCourierMap(
+        courier.entregador.ent_nome, 
+        courier.entregador.entregador_id, 
+        courier.entregador.ent_coordenadas, 
+        this.selectedCouriersState[courier.entregador.ent_nome],
+        courier.entregador.color
+      );
     },
 
+    selectCourierMap(courierName, courierId, courierLocation, courierSelectedCard, courierColor) {
+      console.log(courierName);
+      if ( 
+        courierName === "Sem Entregador" || 
+        courierName === "Sem entregador" || 
+        courierName === "sem Entregador" || 
+        courierName === "sem entregador" ) { 
+          console.log(courierName);
+          courierColor = "#808080"; 
+        } 
 
+      console.log(courierColor);
+      this.selectedCouriersState[courierId] = courierSelectedCard;
+
+      const courierKey = `${courierName}.${courierId}.${courierLocation.lat}.${courierLocation.lng}.${courierColor}`;
+      const courierKeyValidation = `${courierName}.${courierId}.${courierLocation.lat}.${courierLocation.lng}`;
+
+      const validationEntregadorIndex = this.validationEntregador.indexOf(courierKeyValidation);
+
+      if (validationEntregadorIndex !== -1) {
+        this.validationEntregador = this.validationEntregador.filter(item => item !== courierKeyValidation);
+      } else {
+        this.validationEntregador.push(courierKeyValidation);
+        this.selectedMultipleCouriers.push(courierKey);
+      }
+      
+      this.$refs.map.addAllOrdersForDeliverers(this.validationEntregador, courierColor);
+    },
 
     updateMapAccordingToSelection() { 
       this.clearMapWithMarkersAndPolylines();
 
-      if (this.selectedCouriers.length === this.deliverers.length || this.selectedCouriers.length === 0) { 
+      if (this.selectedCouriers.length === this.pedidos.producao.length || this.selectedCouriers.length === 0) { 
         this.$refs.map.addAllOrdersToMap(); 
       } else { 
         this.selectedCouriers.forEach(courier => { 
-          const deliverer = this.deliverers.find(d => d.name === courier); 
+          const deliverer = this.pedidos.producao.find(d => d.entregador.ent_nome === courier); 
           if (deliverer) { 
-            this.$refs.map.addAllOrdersForDeliverers(deliverer); 
+            this.$refs.map.addAllOrdersForDeliverers([deliverer], deliverer.entregador.color);
           } 
         }); 
       } 
     },
 
     getAllOrdersAndDeliverers(){
-      this.selectedCouriers = [];  
-      this.clickInAll = true;
+      this.selectedCouriers = []; 
       this.selectedCouriersState = {};
       this.$refs.map.addAllOrdersToMap();
     },
 
     getAllOrdersWithoutPerson(){
-      this.selectedCouriers = []; 
-      this.clickInWithoutCourier = true;
-      this.selectedCouriersState = {};
       this.$refs.map.searchOrdersWithoutPerson();
     },
-
-    selectCourierMap(courier, courierId, courierLocation, courierColor, courierSelectedCard) {
-      this.selectedCouriersState[courierId] = courierSelectedCard;
-
-      const courierKey = `${courier}.${courierId}.${courierLocation.lat}.${courierLocation.lng}.${courierColor}`;
-
-      const index = this.selectedMultipleCouriers.indexOf(courierKey);
-
-      if (this.clickInAll === false && this.clickInWithoutCourier === false) {
-        if (index === -1) {
-          this.selectedMultipleCouriers.push(courierKey);
-        }
-      } else {
-        if (index !== -1) {
-
-          this.selectedCouriers = [];
-          this.selectedMultipleCouriers = this.selectedMultipleCouriers.filter(item => {
-            item !== courierKey
-          });
-          this.clickInAll = false;
-          this.clickInWithoutCourier = false;
-
-          /*verificar a classe do botão em especifico, senão quando apertar no botão ele continua ativo*/
-
-          this.selectedMultipleCouriers.push(courierKey);
-        } else {
-          
-          this.selectedMultipleCouriers.push(courierKey);
-        }
-      }
-
-      console.log('chamando mapa para entregador especifico');
-      this.$refs.map.addAllOrdersForDeliverers(this.selectedMultipleCouriers);
-    }
-
   },
 };
 </script>
@@ -284,7 +438,20 @@ export default {
     margin-right: 10px;
   }
 
-  main > .couriers_map{
+  .filters{
+    width: 1050px;
+    max-width: 99%;
+    margin: 0 auto;
+  }
+
+  .filters > h3{
+    letter-spacing: 7%;
+    font-size: 27px;
+    font-weight: 400;
+    color: rgb(32, 3, 56);
+  }
+
+  .filter_wrapers,main > .couriers_map{
     display: var(--display-flex);
     flex-direction: row;
     position: relative;
@@ -293,31 +460,138 @@ export default {
     height: 100%;
   }
 
-  @media (width < 580px){
-    .card_couriers {
-      top: 80px;
-      right: 15px;
-      height: 250px; 
-      padding: 15px;
-      width: 240px; 
-    }
+  .filter_single{
+    margin-right: 15px;
+  }
 
-    .card_couriers > button,.couriers_options > button{
-      height: 37px;
+  .filter_single:last-child{
+    margin-right: 0;
+  }
+
+  .filter_single > input{
+    cursor: pointer;
+  }
+
+  label{
+    color: rgb(32, 3, 56);
+  }
+
+  .clear_map{
+    position: relative;
+    top: -5px;
+    left: var(--width-height-checkbox);
+    transition: top 0.2s ease-in;
+  }
+
+  .clear_map:active{
+    top: -3px;
+  }
+
+  @media (width < 1130px){
+    .filters{
+      width: auto;
+      max-width: auto;
+      padding-left: 30px;
+      margin: 0;
     }
   }
 
-  @media (width < 390px){
-    .card_couriers {
-      top: 85px;
-      right: 12px;
-      height: 240px; 
-      padding: 14px;
-      width: 230px; 
+  @media (width < 1000px) {
+    .filters{
+      padding-left: 85px;
     }
 
-    .card_couriers > button,.couriers_options > button{
-      height: 35px;
+    .filter_wrapers,main > .couriers_map{
+      display: flex;
+      flex-direction: column-reverse;
+    }
+
+    .filter_wrapers{
+      display: flex;
+      flex-direction: row;
+    }
+  }
+
+  @media (width < 700px) {
+    .filters{
+      padding-left: 65px;
+    }
+  }
+
+  @media (width < 640px) {
+    .filters{
+      padding-left: 30px;
+    }
+  }
+
+  @media (width < 540px) {
+    .filters{
+      padding-left: 25px;
+    }
+  }
+
+  @media (width < 420px) {
+    .filters{
+      padding-left: var(--width-height-checkbox);
+    }
+
+    .filter_wrapers{
+      display: flex;
+      flex-direction: column;
+    }
+
+    .filter_single:nth-child(1){
+      margin-top: 10px;
+    }
+
+    .filter_single > input{
+      cursor: pointer;
+    }
+
+    .filter_single > input{
+      margin-bottom: 8px;
+    }
+
+    .filter_single > input:last-child{
+      margin-bottom: 0;
+    }
+
+    .clear_map{
+      top: 10px;
+      left: -33%;
+      margin: 15px auto;
+    }
+
+    .clear_map:active{
+      top: 12px;
+    }
+  }
+
+  @media (width < 361px) {
+    input::placeholder{
+      font-size: 14px;
+    }
+
+    .filters{
+      padding-left: 15px;
+    }
+
+    .clear_map{
+      left: -29%;
+    }
+  }
+
+  @media (width < 290px) {
+    input::placeholder{
+      font-size: 12px;
+    }
+
+    .filters{
+      padding-left: 10px;
+    }
+
+    .clear_map{
+      left: -26%;
     }
   }
 </style>
